1306caa753bd224ea298d71da01d1ddeeb52147f
Chris Aquino <chris.aquino@gmail.com>
2018-07-26 10:58:31 -0400

    wraps Router in parens for linebreaks

--- src/index.js
+++ src/index.js
@@ -7,5 +7,9 @@ import {BrowserRouter as Router} from 'react-router-dom';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render((
+<Router>
+  <App />
+</Router>
+), document.getElementById('root'));
 registerServiceWorker();
